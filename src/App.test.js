4df9fd2118829cb9ffa93e6a0a40d843f87f54ce
Jhappy77 <joel.happ@shaw.ca>
2021-02-15 14:55:53 -0700

    Set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import HomePage from './HomePage.tsx';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<HomePage />);
+  const linkElement = screen.getByText(/under construction/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/HomePage.tsx
@@ -0,0 +1,17 @@
+import tsu_logo from './images/logo_white_start.png'
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <img className="homePage__logo" src={tsu_logo}></img>
+      <div className="homePage__description">Tech Start UCalgary is a new club building software projects and exploring tech entrepreneurship.
+      </div>
+      <a className="homePage__cta" href="https://linktr.ee/techstartuofc">Learn more about us!</a>
+
+      <p className="homePage__construction">This website is under construction... come back soon!</p>
+    </div>
+  );
+}
+
+export default HomePage;

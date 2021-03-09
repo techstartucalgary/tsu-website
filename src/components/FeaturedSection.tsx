7284a7d3ea8fcba46975162a30187a75f34f7513
Jhappy77 <joel.happ@shaw.ca>
2021-03-09 10:49:37 -0700

    Refactor section css, add adaptvity

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./FeaturedSection.css";
 
 function FeaturedSection(props:any) {
-  return<article className="homePage__section homePage__spacing">
-      <img src={props.sectionImage} alt="" className="homePage__sectionImage" />
-      <h3 className="homePage__sectionTitle">{props.sectionTitle}</h3>
-      <p className="homePage__sectionDescription">{props.sectionDescription}</p>
-      <a href={props.sectionLink} className="homePage__btn">
+  return<article className="featuredSection">
+      <img src={props.sectionImage} alt="" className="featuredSection__sectionImage" />
+      <h3 className="featuredSection__sectionTitle">{props.sectionTitle}</h3>
+      <p className="featuredSection__sectionDescription">{props.sectionDescription}</p>
+      <a href={props.sectionLink} className="featuredSection__btn">
         Learn More
       </a>
     </article>

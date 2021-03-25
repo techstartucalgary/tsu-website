d569f8e5bf6ab2889a382dd2b5f384aabe7a09bf
zNon18 <stalind14@gmail.com>
2021-03-25 12:26:11 -0600

    Improved Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer/Footer';
-import Header from 'components/Header';
 import './App.css';
 
 function App(){
     return <Router>
         <div className="App">
-            <Header/>
+            <Navbar/>
             <div className="content">
                 <Switch>
                     <Route path="/docs">

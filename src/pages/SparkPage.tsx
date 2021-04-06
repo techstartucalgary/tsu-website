b7f2cdfe6ab1e39cf48144e459edce60df49dcf9
Taylor Noel <taylor.noel1@outlook.com>
2021-04-06 16:31:11 -0600

    add techSpark mission and styling

--- src/pages/SparkPage.tsx
+++ src/pages/SparkPage.tsx
@@ -1,6 +1,7 @@
 import React, { Component, useEffect } from 'react';
 import './SparkPage.css';
 import axios from "axios";
+import '../components/TechSpark/Event'
 
 const dataRoot = "https://techstartbackend.herokuapp.com"
 function Dashboard() {
@@ -15,16 +16,23 @@ return(
     <body>
         
         <div className="techsparkTitle">Tech Spark</div>
-
+        <div className = "page">
+            <div className="techsparkTitle" id = "first">Mission</div>
+            <p className = "techspark_about">TechStart has partnered with the Hunter Hub to create TechSpark where we believe that young entrepreneurs have great potential to create innovative products
+and solutions. Our goal is to help those seeking new opportunities or collaborators by 
+providing a space for individuals to post their ideas, find out more about technolgy 
+focused entrepreneurship events, and connect with like minded individuals looking for 
+specific skill sets.</p>
+        </div>
         
        <div className="page">
-           <div className="techsparkTitle">Whats the latest?:</div>
+           <div className="techsparkTitle">Whats the Latest?</div>
            <div className="sparkGrid"></div>
         </div>
     </body>   
        <br/>
     <div className="page">
-        <div className="techsparkTitle">See what people are saying</div>
+        <div className="techsparkTitle">See What People Are Saying</div>
         <div className="sparkGrid"></div>
     </div>
     </div> 

a5c6dec96e6b5f21dab37e2f3828156996a1d7f0
AmandaNguye <77892730+AmandaNguye@users.noreply.github.com>
2021-04-25 23:34:18 -0600

    Add Project Accordian

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ApplyPage.css";
 import gallery1 from "images/calgary.jpg";
 import gallery2 from "images/banana.jpg";
@@ -7,12 +7,13 @@ import gallery4 from "images/funkystudent.jpg";
 import gallery5 from "images/faketeam.jpg";
 import ApplySection from "components/ApplySection";
 import ApplyFAQSection from "components/ApplyFAQSection";
+import Accordion from "../components/Apply/Accordion";
 
 //import AOS from "../../node_modules/aos";
 //import "aos/dist/aos.css";
 
 function ApplyPage() {
-  //window.addEventListener("load", AOS.refresh);
+  const [active, setActive] = useState("");
   return (
     <div className="applyPage">
       <div className="applyPage__waves applyPage__waves1"></div>
@@ -25,47 +26,92 @@ function ApplyPage() {
           entreprenuerial and software environment at the University of Calgary.
         </p>
       </header>
-      <div className="applyPage__intro">
-        <h1 className="applyPage__SectionTitle">What We Do</h1>
-        <div className="applyPage__introImg"></div>
-        <div className="applyPage__introContents" data-aos="fade-down">
-          <h2>
-            Tech Start is a UofC student student-run software engineering design
-            team devoted to building software projects in a collaborative and
-            professional environment. Our primary objective is to be the best
-            space at UofC for students to find a welcoming community, build
-            projects in a team setting, gain experience with professional tools
-            and principles, and share their knowledge with others.
-          </h2>
-          <button className="applyPage__btn">Join the Community</button>
+      <div className="applyPage__introContainer">
+        <div className="applyPage__intro">
+          <h1 className="applyPage__SectionTitle">What We Do</h1>
+          <div className="applyPage__introImg"></div>
+          <div className="applyPage__introContents" data-aos="fade-down">
+            <h2>
+              Tech Start is a UofC student student-run software engineering
+              design team devoted to building software projects in a
+              collaborative and professional environment. Our primary objective
+              is to be the best space at UofC for students to find a welcoming
+              community, build projects in a team setting, gain experience with
+              professional tools and principles, and share their knowledge with
+              others.
+            </h2>
+            <button className="applyPage__btn">Join the Community</button>
+          </div>
         </div>
       </div>
 
+      <div className="applyPage__container applyPage__projectIntroContainer">
+        <img className="applyPage__projectIntroImg" src={gallery5} alt="" />
+        <div className="applyPage__projectIntro">
+          <h1 className="applyPage__SectionTitle">Projects</h1>
+          <Accordion
+            title="What is a Tech Start project?"
+            active={active}
+            setActive={setActive}
+            description="Every semester, our members form teams that gather together to
+                  brainstorm a project, and then work on that idea
+                  collaboratively over the semester. During this time the
+                  projects evolve and shift, and our members get to experience
+                  the full lifecycle of bringing a complex project to life. Each
+                  team is lead by an experienced tech lead who provides the
+                  expertise needed to execute on the team’s vision. Tech Start
+                  also has mentors for each platform to help you learn to solve
+                  unique problems on your platform. We are also fortunate enough
+                  to be supported by a number of companies who provide us with
+                  the resources necessary to build our projects and run events
+                  and workshops."
+          />
+          <Accordion
+            title="Why join a project?"
+            active={active}
+            setActive={setActive}
+            description=""
+          />
+          <Accordion
+            title="What roles are take part in a project?"
+            active={active}
+            setActive={setActive}
+            description=""
+          />
+          <Accordion
+            title="What is required to take part in a project?"
+            active={active}
+            setActive={setActive}
+            description=""
+          />
+        </div>
+      </div>
+      <div className="applyPage__container"></div>
       <div className="applyPage__container applyPage__applicationContainer">
         <div className="applyPage__application">
           <h1 className="applyPage__SectionTitle">Applications</h1>
           <div className="applyPage__applicationImg"></div>
           <ul className="applyPage__applicationUL">
             <ApplySection
-              term="1 academic year"
-              role="Executive Team"
-              description="Work behind the scenes to organize project teams, run
-                  workshops and events, and more. Lead and grow the club vision
-                  and culture."
+              term="1 semester"
+              role="Project Member"
+              description="Work and grow with other developers on different platforms,
+                  collaborating cross-functionally to build an exciting project."
               applicationLink="#"
             />
             <ApplySection
               term="1 semester"
               role="Project Manager Team"
               description="Manage a project for a semester and lead a team of 6-9
-                  developers to create complex projects."
+                    developers to create complex projects."
               applicationLink="#"
             />
             <ApplySection
-              term="1 semester"
-              role="Project Developer"
-              description="Work and grow with other developers on different platforms,
-                  collaborating cross-functionally to build an exciting project."
+              term="1 academic year"
+              role="Executive Team"
+              description="Work behind the scenes to organize project teams, run
+                  workshops and events, and more. Lead and grow the club vision
+                  and culture."
               applicationLink="#"
             />
           </ul>

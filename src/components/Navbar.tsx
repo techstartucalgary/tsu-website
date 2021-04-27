8f099a3d4b409209230de319665302703ea4731c
Jhappy77 <joel.happ@shaw.ca>
2021-04-26 21:01:18 -0600

    Fix Navbar spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,30 @@
 import "./Navbar.css"
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from "images/tech-start-logo-white.png";
 
-function Header (props: any){
- var temp:string = props.HelloName;
- return <header className="navbar">
+function Header(props: any) {
+    var temp: string = props.HelloName;
+    return <header className="navbar__container">
+        <div className="navbar">
+            <input type="checkbox" id="navbar__nav-toggle" className="navbar__nav-toggle" />
+            <img src={logo} alt="logo" className="navbar__logo" />
+            <nav className="navbar__content">
+                <ul>
+                    <li> <a href="#"><Link to="/" > About</Link></a> </li>
+                    <li> <a href="#"><Link to="/docs" >Docs</Link></a> </li>
+                    <li> <a href="#"><Link to="/" >Projects</Link></a> </li>
+                    <li> <a href="#"><Link to="/" >Get Involved</Link></a> </li>
+                    <li> <a href="#"><Link to="/" >Resources</Link></a> </li>
+                </ul>
+            </nav>
+            <label htmlFor="navbar__nav-toggle" className="navbar__nav-toggle-label">
+                <span></span>
+            </label>
 
+        </div>
+        <div className="navbar__placeholder"></div>
 
-
- <input type="checkbox" id="navbar__nav-toggle" className="navbar__nav-toggle"/>
- <img src ={logo} alt="logo" className="navbar__logo"/>
- <nav className="navbar__content">
- 
-     
-     <ul>
-         <li> <a href ="#"><Link to ="/" > About</Link></a> </li> 
-         <li> <a href ="#"><Link to ="/docs" >Docs</Link></a> </li>
-         <li> <a href ="#"><Link to ="/" >Projects</Link></a> </li>
-         <li> <a href ="#"><Link to ="/" >Get Involved</Link></a> </li>
-         <li> <a href ="#"><Link to ="/" >Resources</Link></a> </li>
-     </ul>
-     
- </nav>
- <label htmlFor="navbar__nav-toggle" className="navbar__nav-toggle-label">
-     <span></span>
-     
-   </label>
-
-</header>  
+    </header>
 }
 export default Header;

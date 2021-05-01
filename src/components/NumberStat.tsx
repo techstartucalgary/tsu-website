d3f82c1497f52251dc2186d332edc5b28cb45c9c
Admin <admin@MacBook-Air-Admin.local>
2021-05-02 05:12:22 +0600

    make apply button link to ApplyPage

--- src/components/NumberStat.tsx
+++ src/components/NumberStat.tsx
@@ -5,11 +5,12 @@ import CountUp from 'react-countup';
 
 
 
+
 function NumberStat(props: any) {
 
     return <div className="numberStat">
         <div className="numberStat__number">
-            <CountUp start={0} end={props.number} duration={1.7} delay={0.2} /></div>    
+            <CountUp start={0} end={props.number} duration={1.7} delay={0.2} /></div>
         <p className="numberStat__stat">{props.stat}</p>
     </div>
 }

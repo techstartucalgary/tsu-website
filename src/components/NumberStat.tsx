864341d29266e5840c586cda8274116212b8abc5
AmandaNguye <77892730+AmandaNguye@users.noreply.github.com>
2021-05-05 10:52:03 -0600

    Add visibility sensor to stats.

--- src/components/NumberStat.tsx
+++ src/components/NumberStat.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import './NumberStat.css';
-import { render } from 'react-dom';
-import CountUp from 'react-countup';
-
-
-
+import React from "react";
+import "./NumberStat.css";
+import { render } from "react-dom";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 
 function NumberStat(props: any) {
-
-    return <div className="numberStat">
-        <div className="numberStat__number">
-            <CountUp start={0} end={props.number} duration={1.7} delay={0.2} /></div>
-        <p className="numberStat__stat">{props.stat}</p>
+  return (
+    <div className="numberStat__number">
+      <VisibilitySensor>
+        {({ isVisible }) => (
+          <div className="numberStat__number">
+            {(isVisible) ? (
+              <CountUp
+                start={0}
+                end={props.number}
+                duration={2.0}
+                delay={0.2}
+              />
+            ) : (
+              <CountUp start={0} end={0} duration={0} delay={0} />
+            )}
+          </div>
+        )}
+      </VisibilitySensor>
+      <p className="numberStat__stat">{props.stat}</p>
     </div>
+  );
 }
 
 export default NumberStat;

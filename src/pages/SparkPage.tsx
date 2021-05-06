0a1b80e98b50f495ac2689701e54c0438f9c3830
Richard <51135299+rzwc@users.noreply.github.com>
2021-05-06 00:05:20 -0600

    Connect back end events and posts to front end

--- src/pages/SparkPage.tsx
+++ src/pages/SparkPage.tsx
@@ -8,22 +8,23 @@ import SignIn from '../components/TechSpark/SignIn'
 import Popup from "components/TechSpark/Popup"
 import Register from '../components/TechSpark/Register'
 import MenuButton from "components/TechSpark/MenuButton"
+import Post from "components/TechSpark/post"
 import { EventType } from "./EventType"
-
-const dataRoot = "https://techstartbackend.herokuapp.com"
+import { PostType } from "./PostType"
 
 function Dashboard() {
 
-const [newsCategories, setNewsCategories] = React.useState([]);
+    const [newsCategories, setNewsCategories] = React.useState([]);
 
-const [news, setNews] = React.useState([])
+    const [news, setNews] = React.useState([])
     
-   
     const dataRoot = "https://techstartbackend.herokuapp.com"
     const [token, setToken] = React.useState([]);
     const [userID, setUserID] = React.useState([]);
 
     const [events, setEvents] = React.useState([])
+
+    const [posts, setPosts] = React.useState([])
 
     //Get existing news 
     useEffect(() => {
@@ -31,28 +32,59 @@ const [news, setNews] = React.useState([])
             `${dataRoot}/news`
           ).then((response) => {
             if (response.status === 200) {
-                
                 let data = response.data;
                 setEvents(data)
-                let event = new EventType(data[0])
-                console.log(event)
-                console.log(data)
-                console.log(JSON.parse(data))
           }}, (error) => {
             console.log(error);
           });; 
     },[])
-    console.log(newsCategories)
-    console.log(events)
-    console.log(events.length)
-    console.log(events[0])
-    console.log(events[0])
-    // if (events.length > 0) {
-    //     console.log(events[0].category)
-    // }
     
+    //Get existing news 
+    useEffect(() => {
+        axios.get(
+            `${dataRoot}/post`
+          ).then((response) => {
+            if (response.status === 200) {
+                let data = response.data;
+                setPosts(data)
+          }}, (error) => {
+            console.log(error);
+          });; 
+    },[])
+    
+    console.log(posts)
+    const eventArray = []
+    const postArray = []
+    for (let index=0; index<events.length; index++) {
+        let eventWithType = new EventType(events[index])
+        eventArray.push(eventWithType)
+    }
 
-  useEffect(() => {
+    const eventList = eventArray.map((ev) => 
+        <Event 
+            name = {ev.title} 
+            date = {ev.date_published}
+            img = {ev.url} 
+            link = {ev.source}
+            description = {ev.news_description}
+        />
+    )
+
+
+    for (let index=0; index<posts.length; index++) {
+        let postWithType = new PostType(posts[index])
+        postArray.push(postWithType)
+    }
+
+    const postList = postArray.map((pst) => 
+        <Post
+            title = {pst.title} 
+            date = {pst.date_posted}
+            author = {pst.author}
+            description = {pst.post_description}
+        />
+    )
+    useEffect(() => {
         axios.post(
             `${dataRoot}/login`, {
                 username: 'test@testing.test',
@@ -92,24 +124,6 @@ const [news, setNews] = React.useState([])
         });; 
     },[])
 
-    useEffect(() => {
-        axios.post(
-            `${dataRoot}/login`, {
-                username: 'test@testing.test',
-                password: 'WordPass123'
-            }
-          ).then((response) => {
-                
-                let token = response.data.token;
-                let userID = response.data.id;
-                setToken(token)
-                setUserID(userID)
-                console.log(token)
-                
-          }, (error) => {
-            console.log(error);
-          });; 
-    },[])
 
     const [displayPost,setDisplayPost] = useState(false)
     //States: PostCreation(1), SignIn(2)
@@ -153,12 +167,7 @@ const [news, setNews] = React.useState([])
        <div className="sparkContentPage">
            <div className="sparkContentTitle">Whats the latest?:</div>
            <div className="sparkInnerGrid">
-               <Event/>
-               <Event/>
-               <Event/>
-               <Event/>
-               <Event/>
-               <Event/>
+           {eventList}
            </div>
         </div>
         <div className="sparkContentPage">
@@ -166,6 +175,7 @@ const [news, setNews] = React.useState([])
             See what people are saying:
             <div className="sparkInnerGrid">
             <PostButton showPost={showPost} showPostCreation={showPostCreation}/>
+            {postList}
             </div>
         </div>
     </div>

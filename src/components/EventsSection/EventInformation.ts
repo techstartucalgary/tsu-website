8c9374a57cef42dbc4d4e9ef871dff03d5b5a8dd
Jhappy77 <joel.happ@shaw.ca>
2021-05-13 14:20:32 -0600

    Update event descriptions

--- src/components/EventsSection/EventInformation.ts
+++ src/components/EventsSection/EventInformation.ts
@@ -9,7 +9,7 @@ export var pastEvents: Event[];
 upcomingEvents = [
     {
         title: "Alberta's Tech Horizon",
-        description: "Alberta’s Tech Horizon is a panel event focused on the past, present and future of the technology industry in Alberta. Join us in our first ever panel event as we speak with: Laura Buhler(Executive Director of the C100), Kelly Cherniwchan(CEO and Founder of Chata Technologies), Jay Gohill(President and CEO of Arcurve), Kris Read(Co- Founder and Head of Engineering at Neo Financial).",
+        description: "Alberta’s Tech Horizon is a panel event focused on the past, present and future of the technology industry in Alberta. Our panelists are Laura Buhler (Executive Director of the C100), Kelly Cherniwchan (CEO and Founder of Chata Technologies), Jay Gohill (President and CEO of Arcurve), and Kris Read (Co-Founder and Head of Engineering at Neo Financial). We hope to see you there!",
         date: "Thu, May 13, 2021",
         time: "7:30 PM – 8:45 PM MDT",
         image: Alberta_Tech_Horizon_img,
@@ -20,7 +20,7 @@ upcomingEvents = [
 pastEvents = [
     {
         title: "Building a Brand and Business",
-        description: "For Tech Fest, Tech Start is excited to present a webinar on founding a business. Judy Hughes will walk us through her experiences with founding her own company JYZ Design and discuss what business practices and strategies companies will use to stand out from their competitors.",
+        description: "As part of the Hunter Hub's Tech Bytes event, we collaborated with entrepreneur Judy Hughes to host a webinar on building a brand and a business. Judy walked us through her experiences with founding her own company, JYZ Design, and discussed how companies can stand out from their competitors.",
         date: "",
         time: "",
         image: Building_a_Brand_and_Business_img,

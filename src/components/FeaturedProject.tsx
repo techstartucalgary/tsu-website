ce9de3270ec07b07c61cb6d6012527192bae13e0
Jhappy77 <joel.happ@shaw.ca>
2021-05-13 14:36:05 -0600

    Update project styling, remove old code

--- src/components/FeaturedProject.tsx
+++ src/components/FeaturedProject.tsx
@@ -4,9 +4,6 @@ import { useTheme } from "styled-components";
 import './FeaturedProject.css'
 import ProjectSection from "./ProjectSection";
 
-/**
- * TODO: Re-implement
- */
 
 interface featuredProjectProps {
   project: Project
@@ -23,7 +20,6 @@ function FeaturedProject(props: featuredProjectProps) {
   function mouseEnter() {
     setProjectImageClass("project__image--transparent")
     setprojectDescriptionClass("project__description--visible")
-
   }
   return <div className="project__container" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
     <img src={props.project.image} className={"project__image " + projectImageClass} />
@@ -33,22 +29,6 @@ function FeaturedProject(props: featuredProjectProps) {
     </div>
 
   </div>
-  {/*<a href="https://github.com/Tech-Start-UCalgary/tsu-website" className="featuredProjectSection">
-    <div className="featuredProject">
-      <img className="featuredProjectImage" src={props.project.image}></img>
-
-      <div className="featuredProjectDescription">{props.project.description}</div>
-    </div>
-
-</a>*/}
-
-  // return<a href={props.projectLink} className="projectItem">
-  //     <img src={props.projectImage} alt=""/>
-  //     <p className="projectItem__Details">
-  //       <span className="">{props.projectName}</span>
-  //       {props.teamName}
-  //     </p>
-  //   </a>;
 }
 
 export default FeaturedProject;

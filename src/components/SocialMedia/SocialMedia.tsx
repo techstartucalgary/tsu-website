1af6b083598bc2909242a5805c61deedc3ba39ab
Jhappy77 <joel.happ@shaw.ca>
2021-06-06 15:24:27 -0600

    Clean up social media component

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -16,30 +16,12 @@ interface SocialMediaProps {
 
 const SocialMedia = (props: SocialMediaProps) => {
     const socialMediaClasses = ["socialMediaIcon"];
-    // if (props.footer) SocialMediaClasses.push("SocialMediaIcon-FooterColor");
-    // else SocialMediaClasses.push("SocialMediaIcon-DefaultColor");
-    // if (props.noHoverColor) SocialMediaClasses.push("SocialMediaIcon--NoHovorColor")
-    let additionalIconStyles = {};
     switch (props.color) {
         case (SocialMediaColor.Green):
             socialMediaClasses.push("socialMediaIcon--green");
             break;
         case (SocialMediaColor.Gradient):
             socialMediaClasses.push("socialMediaIcon--gradient");
-            additionalIconStyles = {
-                // "color": "red",
-                // "background": "linear-gradient(45deg, var(--secondary-lime), var(--secondary-blue))",
-                // "-webkit-background-clip": "text",
-                // "-moz-background-clip": "text",
-                // "-webkit-text-fill-color": "transparent",
-                // "-moz-text-fill-color": "transparent",
-                // "color": "#9c47fc",
-                // "display": "block",
-                // "background": "-webkit-linear-gradient(#9c47fc, #356ad2)",
-                // "-webkit-background-clip": "text",
-                // "-webkit-text-fill-color": "transparent",
-                // "fill": "url(#customSVGFromIndexDotHTML)"
-            };
             break;
         case (SocialMediaColor.Black):
             break;
@@ -51,7 +33,7 @@ const SocialMedia = (props: SocialMediaProps) => {
         <a
             href={props.link}
             className={socialMediaClasses.join(' ')}>
-            <FontAwesomeIcon icon={props.icon} size="2x" style={additionalIconStyles} />
+            <FontAwesomeIcon icon={props.icon} size="2x"/>
         </a>
     );
 };

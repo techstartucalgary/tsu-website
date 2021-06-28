adb38fb06fc1b50234f98d075555758c637256ea
Niyousha99 <niyousha.raeesinejad@ucalgary.ca>
2021-06-27 18:50:59 -0600

    Update events and fix margins in event section

--- src/components/EventsSection/EventInformation.ts
+++ src/components/EventsSection/EventInformation.ts
@@ -7,6 +7,10 @@ export var upcomingEvents: Event[];
 export var pastEvents: Event[];
 
 upcomingEvents = [
+
+];
+
+pastEvents = [
     {
         title: "Alberta's Tech Horizon",
         description: "Alberta’s Tech Horizon is a panel event focused on the past, present and future of the technology industry in Alberta. Our panelists are Laura Buhler (Executive Director of the C100), Kelly Cherniwchan (CEO and Founder of Chata Technologies), Jay Gohill (President and CEO of Arcurve), and Kris Read (Co-Founder and Head of Engineering at Neo Financial). We hope to see you there!",
@@ -14,10 +18,8 @@ upcomingEvents = [
         time: "7:30 PM – 8:45 PM MDT",
         image: Alberta_Tech_Horizon_img,
         link: "https://www.eventbrite.ca/e/albertas-tech-horizon-tickets-149515875007"
-    }
-];
+    },
 
-pastEvents = [
     {
         title: "Building a Brand and Business",
         description: "As part of the Hunter Hub's Tech Bytes event, we collaborated with entrepreneur Judy Hughes to host a webinar on building a brand and a business. Judy walked us through her experiences with founding her own company, JYZ Design, and discussed how companies can stand out from their competitors.",

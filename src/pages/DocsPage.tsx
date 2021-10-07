038cb522d125d17d592d87c5d98fb4ac58028c77
Nurgul <nurgulakhsh@gmail.com>
2021-10-07 11:58:32 -0600

    updated team section

--- src/pages/DocsPage.tsx
+++ src/pages/DocsPage.tsx
@@ -24,8 +24,8 @@ function DocsPage() {
 
       <ul className="sectionContent">
         <li><Emoji symbol="👩🏽‍💻 " />Club Name: Refer to the club as "Tech Start UCalgary" or "Tech Start", but avoid the acronyms "TS" and "TSU".</li>
-        <li><Emoji symbol="🖥️ " />Logos: Want to use the Tech Start logo? You can download official versions from our Google Drive <a href="https://drive.google.com/drive/folders/1VW7TTFteVvLU4avbyZvtdizZ9fdNhYRC?usp=sharing" target="_blank">here</a>!</li>
-        <li><Emoji symbol="🔖 " />Styling Guide: You can find a more in-depth styling guide <a href="#" target="_blank">here</a>.</li>
+        <li><Emoji symbol="🖥️ " />Logos: Want to use the Tech Start logo? You can download official versions from our Google Drive <a href="https://drive.google.com/drive/u/2/folders/1xYYzdgP9Asac6FP_omsDuCq0mEoaE8UY" target="_blank">here</a>! Please note that you can download the logos, but not delete or alter them.</li>
+        {/*<li><Emoji symbol="🔖 " />Styling Guide: You can find a more in-depth styling guide <a href="#" target="_blank">here</a>.</li> */}
       </ul>
     </section>;
   }

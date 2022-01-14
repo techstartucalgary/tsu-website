3a2f0cf96be2d7117c26c75209f0d1e2a7c98009
Niyousha99 <niyousha.raeesinejad@ucalgary.ca>
2022-01-13 21:08:17 -0700

    Add galatea sponsor

--- src/components/SponsorSection/SponsorsInfo.ts
+++ src/components/SponsorSection/SponsorsInfo.ts
@@ -2,6 +2,7 @@ import sponsorLogoHunterHub from "images/sponsor-logo-hunter-hub.png";
 import sponsorLogoArcurve from "images/sponsor-logo-arcurve.png";
 import sponsorLogoUnity from "images/sponsor-logo-unity.png";
 import sponsorLogoMicrosoft from "images/sponsor-logo-microsoft.png";
+import sponsorGalatea from "images/sponsor-logo-galatea.png";
 
 export type Sponsor = {
   sponsorLink: string;
@@ -10,19 +11,24 @@ export type Sponsor = {
 
 export const SponsorList: Sponsor[] = [
   {
-    sponsorLink: "https://www.arcurve.com/",
-    sponsorImage: sponsorLogoArcurve,
-  },
-  {
-    sponsorLink: "https://www.ucalgary.ca/hunter-hub",
-    sponsorImage: sponsorLogoHunterHub,
-  },
-  {
     sponsorLink: "https://www.microsoft.com/",
     sponsorImage: sponsorLogoMicrosoft,
   },
   {
+    sponsorLink: "https://galateatech.com/",
+    sponsorImage: sponsorGalatea,
+  },
+  {
+    sponsorLink: "https://www.arcurve.com/",
+    sponsorImage: sponsorLogoArcurve,
+  },
+  {
     sponsorLink: "https://unity.com/",
     sponsorImage: sponsorLogoUnity,
+  },
+
+  {
+    sponsorLink: "https://www.ucalgary.ca/hunter-hub",
+    sponsorImage: sponsorLogoHunterHub,
   },
 ];

2dd3e3a861e60e9ab089b3216c7853753025ff2f
Niyousha99 <niyousha.raeesinejad@ucalgary.ca>
2022-04-04 21:52:41 -0600

    Fixed toggle button (needs to be styled)

--- src/components/TeamSection/TeamSection.styles.ts
+++ src/components/TeamSection/TeamSection.styles.ts
@@ -17,3 +17,50 @@ export const TeamSection = styled.div`
     }
   }
 `;
+
+export const ToggleButtonWrapper = styled.div`
+  position: relative;
+`;
+
+export const ToggleButtonLabel = styled.label`
+  overflow-y: hidden;
+  display: inline-block;
+  position: absolute;
+  width: 300px;
+  height: 50px;
+  border-radius: 15px;
+  background: #bebebe;
+  cursor: pointer;
+  &::after {
+    transform: translateY(-35px);
+    content: "";
+    display: block;
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3px;
+    background: #ffffff;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+  }
+`;
+
+export const ToggleButton = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 42px;
+  height: 26px;
+  &:checked + ${ToggleButtonLabel} {
+    background: #4fbe79;
+    &::after {
+      content: "";
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+      transform: translateY(-35px);
+    }
+  }
+`;
--- src/components/TeamSection/TeamSection.tsx
+++ src/components/TeamSection/TeamSection.tsx
@@ -1,32 +1,36 @@
-import React, { ChangeEvent } from "react";
+import React from "react";
 import { useState } from "react";
 import Team from "./Team";
 import useViewport from "../UseViewport";
 import * as S from "./TeamSection.styles";
 import { executiveTeam, projectManagers } from "./TeamInformation";
-import Divider from "components/Divider";
 
 const TeamSection = () => {
-  const { width } = useViewport(); // get screen width
   const [toggle, setToggled] = useState(false);
   const handleChange = () => setToggled(!toggle);
+
   // set defaultView flag according to screen width cutoff value
+  const { width } = useViewport(); // get screen width
   let defaultView = width > 600 ? true : false;
 
   return (
-    <div>
-      <span className="team_switch">
-        <label className="team_switch_label">
-          {toggle ? (
-            <Team teamMembers={projectManagers} desktopView={defaultView} />
-          ) : (
-            <Team teamMembers={executiveTeam} desktopView={defaultView} />
-          )}
-          <input type="checkbox" onClick={() => handleChange()}></input>
-          <span className="team_switch_span"></span>
-        </label>
-      </span>
-    </div>
+    <S.TeamSection>
+      <S.ToggleButtonWrapper>
+        <S.ToggleButton
+          id="checkbox"
+          type="checkbox"
+          onClick={() => handleChange()}
+        />
+        <S.ToggleButtonLabel htmlFor="checkbox">
+          {toggle ? <p>Project Managers</p> : <p>Executives</p>}
+        </S.ToggleButtonLabel>
+      </S.ToggleButtonWrapper>
+      {toggle ? (
+        <Team teamMembers={projectManagers} desktopView={defaultView} />
+      ) : (
+        <Team teamMembers={executiveTeam} desktopView={defaultView} />
+      )}
+    </S.TeamSection>
   );
 };
 

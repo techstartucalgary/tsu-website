f0a225f389e59e3cddd992b8bf5f1cede224ea07
Sahiti Akella <sahiti.akella@gmail.com>
2022-09-15 20:21:51 -0600

    Closed exec applications

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -265,9 +265,9 @@ const ApplyPage = () => {
               role="Executive Team"
               description="Work behind the scenes for 1 academic year to organize project teams, run
                   workshops and events, and grow our club culture. Be a visionary that helps this club fulfill its goals!"
-              statusIsOpen={true}
-              closedStatus=""
-              applicationLink="https://docs.google.com/forms/d/e/1FAIpQLSezTaetPK1ghEbmwPWK5DqJO3CBigKzdnKqqDviO7XFokBC4A/viewform"
+              statusIsOpen={false}
+              closedStatus="Recruitment Finished"
+              applicationLink=""
             />
           </ul>
         </div>

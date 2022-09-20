bf635ed0fa4dc4d8e0346d438186df3ac705840f
Niyousha Raeesinejad <59885733+Niyousha99@users.noreply.github.com>
2022-09-20 09:23:54 -0600

    Apply page restructure (#374)
    * Updated Exec Photo (#361)
    
    * Update noteaid project pic
    
    * Update content, add maintenance guide, and some file restructuring
    
    * Update README.md
    
    * Update apply page with info for upcoming academic year
    
    * Update exec application date
    
    * Update README.md
    
    * Updated Mya's Picture
    
    Co-authored-by: Niyousha Raeesinejad <59885733+Niyousha99@users.noreply.github.com>
    Co-authored-by: Niyousha99 <niyousha.raeesinejad@ucalgary.ca>
    Co-authored-by: Jhappy77 <joel.happ@shaw.ca>
    Co-authored-by: Joel <45266424+Jhappy77@users.noreply.github.com>
    
    * Close 2022-2023 PM Applications (#366)
    
    * Updated team profiles for 2022-2023 (#368)
    
    * Updated team profiles for 2022-2023
    
    * Fixed profile alignment
    
    * Open Exec Applications (VP External + Events) (#369)
    
    Opened VP External + Jr VP Events Application
    
    * Application Updates (#371)
    
    * Closed exec applications
    
    * Opened project member applications
    
    * Apply page restructure (#373)
    
    * Project member applications  (#372)
    
    * Updated Exec Photo (#361)
    
    * Update noteaid project pic
    
    * Update content, add maintenance guide, and some file restructuring
    
    * Update README.md
    
    * Update apply page with info for upcoming academic year
    
    * Update exec application date
    
    * Update README.md
    
    * Updated Mya's Picture
    
    Co-authored-by: Niyousha Raeesinejad <59885733+Niyousha99@users.noreply.github.com>
    Co-authored-by: Niyousha99 <niyousha.raeesinejad@ucalgary.ca>
    Co-authored-by: Jhappy77 <joel.happ@shaw.ca>
    Co-authored-by: Joel <45266424+Jhappy77@users.noreply.github.com>
    
    * Close 2022-2023 PM Applications (#366)
    
    * Updated team profiles for 2022-2023 (#368)
    
    * Updated team profiles for 2022-2023
    
    * Fixed profile alignment
    
    * Open Exec Applications (VP External + Events) (#369)
    
    Opened VP External + Jr VP Events Application
    
    * Application Updates (#371)
    
    * Closed exec applications
    
    * Opened project member applications
    
    Co-authored-by: Sahiti Akella <63627104+Sahitiakella@users.noreply.github.com>
    Co-authored-by: Jhappy77 <joel.happ@shaw.ca>
    Co-authored-by: Joel <45266424+Jhappy77@users.noreply.github.com>
    
    * Apply page text
    
    * Restructure Apply page for easier access to applications
    
    Co-authored-by: Sahiti Akella <63627104+Sahitiakella@users.noreply.github.com>
    Co-authored-by: Jhappy77 <joel.happ@shaw.ca>
    Co-authored-by: Joel <45266424+Jhappy77@users.noreply.github.com>
    
    Co-authored-by: Sahiti Akella <63627104+Sahitiakella@users.noreply.github.com>
    Co-authored-by: Jhappy77 <joel.happ@shaw.ca>
    Co-authored-by: Joel <45266424+Jhappy77@users.noreply.github.com>

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -92,6 +92,65 @@ const ApplyPage = () => {
           </div>
         </div>
       </div>
+      <div className="applyPage__projectExample">
+        <div className="applyPage__projectExampleText">
+          <h1>
+            Join a project to make new friends, gain new skills, and supercharge
+            your portfolio!
+          </h1>
+        </div>
+        <img src={projectExample} alt="The Tech Start website team" />
+      </div>
+
+      <div className="applyPage__container applyPage__applicationContainer">
+        <div className="applyPage__application">
+          <Blobbie
+            id={2}
+            width={290}
+            transform="translate(clamp(90vw, calc(5vw + 1rem), 40vw), 20%)"
+            edge={false}
+          />
+          <Blobbie
+            id={3}
+            width={200}
+            transform="translate(clamp(-70vw, calc(5vw + 1rem), 40vw), -15%)"
+            edge={false}
+          />
+
+          <h1 className="chonkyHeading chonkyHeading--dynamic chonkyHeading--lessMargin">
+            Applications
+          </h1>
+          <Divider />
+          <ul className="applyPage__applicationUL">
+            <ApplySection
+              role="Project Member"
+              description="Work and grow as a developer, business strategist, or designer.
+                  Collaborate with team members on various platforms to build an exciting project for 1 academic year."
+              statusIsOpen={true}
+              closedStatus="Opens September 2022"
+              applicationLink="https://docs.google.com/forms/d/e/1FAIpQLScB5pRpZNwYsVQTU9spMb4SnclbepceMH3FhU43NOpuU2GCVg/viewform"
+            />
+
+            <ApplySection
+              role="Project Manager"
+              description="Manage a project for 1 year and lead a team of 6-9
+                    project members to create complex projects."
+              statusIsOpen={false}
+              closedStatus="Opens Spring 2023"
+              applicationLink="https://docs.google.com/forms/d/e/1FAIpQLScHj9FcIK4ApPqyMiXf_PLlQ43FpI7EB5gmz7SJFGczU9xIkg/viewform"
+            />
+
+            <ApplySection
+              role="Executive Team"
+              description="Work behind the scenes for 1 academic year to organize project teams, run
+                  workshops and events, and grow our club culture. Be a visionary that helps this club fulfill its goals!"
+              statusIsOpen={false}
+              closedStatus="Recruitment Finished"
+              applicationLink=""
+            />
+          </ul>
+        </div>
+      </div>
       <div className="applyPage__container applyPage__projectIntroContainer">
         <div className="applyPage__projectIntroImg">
           <Lottie options={applyPageTeamLottieOptions} />
@@ -205,71 +264,6 @@ const ApplyPage = () => {
               the entrepreneurial and inventive vision for the community. More
               information on unique roles is in our application!"
           />
-        </div>
-        <div className="applyPage__container applyPage__projectExample">
-          <div className="applyPage__projectExampleText">
-            <h1>
-              Join a project to make new friends, gain new skills, and
-              supercharge your portfolio!
-            </h1>
-          </div>
-          <img src={projectExample} alt="The Tech Start website team" />
-        </div>
-        <Blobbie
-          id={2}
-          width={130}
-          transform="translate(-30%, 70%)"
-          edge={false}
-        />
-      </div>
-
-      <div className="applyPage__container applyPage__applicationContainer">
-        <div className="applyPage__application">
-          <Blobbie
-            id={2}
-            width={290}
-            transform="translate(clamp(90vw, calc(5vw + 1rem), 40vw), 20%)"
-            edge={false}
-          />
-          <Blobbie
-            id={3}
-            width={200}
-            transform="translate(clamp(-70vw, calc(5vw + 1rem), 40vw), -15%)"
-            edge={false}
-          />
-
-          <h1 className="chonkyHeading chonkyHeading--dynamic chonkyHeading--lessMargin">
-            Applications
-          </h1>
-          <Divider />
-          <ul className="applyPage__applicationUL">
-            <ApplySection
-              role="Project Member"
-              description="Work and grow as a developer, business strategist, or designer.
-                  Collaborate with team members on various platforms to build an exciting project for 1 academic year."
-              statusIsOpen={true}
-              closedStatus="Opens September 2022"
-              applicationLink="https://docs.google.com/forms/d/e/1FAIpQLScB5pRpZNwYsVQTU9spMb4SnclbepceMH3FhU43NOpuU2GCVg/viewform"
-            />
-
-            <ApplySection
-              role="Project Manager"
-              description="Manage a project for 1 year and lead a team of 6-9
-                    project members to create complex projects."
-              statusIsOpen={false}
-              closedStatus="Opens Spring 2023"
-              applicationLink="https://docs.google.com/forms/d/e/1FAIpQLScHj9FcIK4ApPqyMiXf_PLlQ43FpI7EB5gmz7SJFGczU9xIkg/viewform"
-            />
-
-            <ApplySection
-              role="Executive Team"
-              description="Work behind the scenes for 1 academic year to organize project teams, run
-                  workshops and events, and grow our club culture. Be a visionary that helps this club fulfill its goals!"
-              statusIsOpen={false}
-              closedStatus="Recruitment Finished"
-              applicationLink=""
-            />
-          </ul>
         </div>
       </div>
     </div>

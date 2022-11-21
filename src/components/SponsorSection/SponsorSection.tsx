6099b9a5cc523b6a959ea8c4113c5455af9f5af4
Niyousha99 <niyousha.raeesinejad@ucalgary.ca>
2022-11-21 12:58:54 -0700

    Update sponsorship package for 2022-2023

--- src/components/SponsorSection/SponsorSection.tsx
+++ src/components/SponsorSection/SponsorSection.tsx
@@ -40,7 +40,7 @@ const SponsorSection = () => {
         <S.SponsorButtonDiv>
           <HoverButton
             mode={ButtonMode.DARK}
-            link="https://drive.google.com/file/d/1Es9AvtbGnGbvhsyh1Vp35qr8DptDiSMv/view"
+            link="https://drive.google.com/file/d/1QHJ0AYNHroMncv1CZzIaSBZaf2TZtN1i/view?usp=sharing"
             linkIsInternal={false}
             text="Check out our sponsorship package!"
           />

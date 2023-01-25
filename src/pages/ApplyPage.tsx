dad38e3abd344530c5fa6ad92ab0d35193da4bdd
Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>
2023-01-24 20:22:10 -0700

    Merge conflicts
    Signed-off-by: Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -264,6 +264,71 @@ const ApplyPage = () => {
               information on unique roles is in our application!"
           />
         </div>
+        <div className="applyPage__container applyPage__projectExample">
+          <div className="applyPage__projectExampleText">
+            <h1>
+              Join a project to make new friends, gain new skills, and
+              supercharge your portfolio!
+            </h1>
+          </div>
+          <img src={projectExample} alt="The Tech Start website team" />
+        </div>
+        <Blobbie
+          id={2}
+          width={130}
+          transform="translate(-30%, 70%)"
+          edge={false}
+        />
+      </div>
+
+      <div className="applyPage__container applyPage__applicationContainer">
+        <div className="applyPage__application">
+          <Blobbie
+            id={2}
+            width={290}
+            transform="translate(clamp(90vw, calc(5vw + 1rem), 40vw), 20%)"
+            edge={false}
+          />
+          <Blobbie
+            id={3}
+            width={200}
+            transform="translate(clamp(-70vw, calc(5vw + 1rem), 40vw), -15%)"
+            edge={false}
+          />
+
+          <h1 className="chonkyHeading chonkyHeading--dynamic chonkyHeading--lessMargin">
+            Applications
+          </h1>
+          <Divider />
+          <ul className="applyPage__applicationUL">
+            <ApplySection
+              role="Project Member"
+              description="Work and grow as a developer, business strategist, or designer.
+                  Collaborate with team members on various platforms to build an exciting project for 1 academic year."
+              statusIsOpen={false}
+              closedStatus="Opens September 2022"
+              applicationLink="https://forms.gle/yxVK3NRCyTjn1TxQ7"
+            />
+
+            <ApplySection
+              role="Project Manager"
+              description="Manage a project for 1 year and lead a team of 6-9
+                    project members to create complex projects."
+              statusIsOpen={false}
+              closedStatus="Opens Spring 2023"
+              applicationLink="https://docs.google.com/forms/d/e/1FAIpQLScHj9FcIK4ApPqyMiXf_PLlQ43FpI7EB5gmz7SJFGczU9xIkg/viewform"
+            />
+
+            <ApplySection
+              role="Executive Team"
+              description="Work behind the scenes for 1 academic year to organize project teams, run
+                  workshops and events, and grow our club culture. Be a visionary that helps this club fulfill its goals!"
+              statusIsOpen={true}
+              closedStatus=""
+              applicationLink="https://docs.google.com/forms/d/e/1FAIpQLSezTaetPK1ghEbmwPWK5DqJO3CBigKzdnKqqDviO7XFokBC4A/viewform"
+            />
+          </ul>
+        </div>
       </div>
     </div>
   );

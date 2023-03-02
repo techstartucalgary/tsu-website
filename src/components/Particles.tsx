0a5f2e1e2c2d1db19e49d8163328266960fd4c97
Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>
2023-03-02 10:18:15 -0700

    update some other dependencies
    Signed-off-by: Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -1,123 +1,156 @@
-import React from "react";
-import Particles from "react-particles-js";
-import "./Particles.css";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
 
-function Particle() {
+const particleOptions = {
+  particles: {
+    number: {
+      value: 125,
+      density: {
+        enable: true,
+        value_area: 641.3648243462092,
+      },
+    },
+    color: {
+      value: "#17EFC8",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#ffffff",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 0,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#17EFC8",
+      opacity: 0.7455866083024681,
+      width: 1.603412060865523,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "bounce",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 7776.548495197786,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "bubble",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 170.53621458328246,
+        size: 28.42270243054708,
+        duration: 0.6496617698410762,
+        opacity: 0.162415442460269,
+        /*speed: 3,*/
+      },
+      repulse: {
+        distance: 150,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: false,
+};
+
+const particleStyle = {
+  zIndex: 1,
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  left: 0,
+  margin: 0,
+  width: "100%",
+  height: "100%",
+};
+
+const Particle = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+
   return (
     <Particles
-      className="particle_container"
-      params={{
-        particles: {
-          number: {
-            value: 125,
-            density: {
-              enable: true,
-              value_area: 641.3648243462092,
-            },
-          },
-          color: {
-            value: "#17EFC8",
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#ffffff",
-            },
-            polygon: {
-              nb_sides: 5,
-            },
-            image: {
-              src: "img/github.svg",
-              width: 100,
-              height: 100,
-            },
-          },
-          opacity: {
-            value: 0.5,
-            random: false,
-            anim: {
-              enable: false,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
-            },
-          },
-          size: {
-            value: 0,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 40,
-              size_min: 0.1,
-              sync: false,
-            },
-          },
-          line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#17EFC8",
-            opacity: 0.7455866083024681,
-            width: 1.603412060865523,
-          },
-          move: {
-            enable: true,
-            speed: 3,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "bounce",
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 7776.548495197786,
-              rotateY: 1200,
-            },
-          },
-        },
-        interactivity: {
-          detect_on: "canvas",
-          events: {
-            onhover: {
-              enable: true,
-              mode: "repulse",
-            },
-            onclick: {
-              enable: true,
-              mode: "bubble",
-            },
-            resize: true,
-          },
-          modes: {
-            grab: {
-              distance: 400,
-              line_linked: {
-                opacity: 1,
-              },
-            },
-            bubble: {
-              distance: 170.53621458328246,
-              size: 28.42270243054708,
-              duration: 0.6496617698410762,
-              opacity: 0.162415442460269,
-              /*speed: 3,*/
-            },
-            repulse: {
-              distance: 150,
-              duration: 0.4,
-            },
-            push: {
-              particles_nb: 4,
-            },
-            remove: {
-              particles_nb: 2,
-            },
-          },
-        },
-        retina_detect: false,
-      }}
+      id="tsparticles"
+      options={particleOptions}
+      style={particleStyle}
+      init={particlesInit}
+      loaded={particlesLoaded}
     />
   );
-}
+};
 
 export default Particle;

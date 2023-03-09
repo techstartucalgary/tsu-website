dc35ea235663aa1016d1ffda3af648dfa6b47739
Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>
2023-03-09 15:04:29 -0700

    Fix dependency conflits, switch to yarn
    Signed-off-by: Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -1,129 +1,7 @@
 import { useCallback } from "react";
-import Particles from "react-tsparticles";
 import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-
-const particleOptions = {
-  particles: {
-    number: {
-      value: 125,
-      density: {
-        enable: true,
-        value_area: 641.3648243462092,
-      },
-    },
-    color: {
-      value: "#17EFC8",
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#ffffff",
-      },
-      polygon: {
-        nb_sides: 5,
-      },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100,
-      },
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
-      },
-    },
-    size: {
-      value: 0,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#17EFC8",
-      opacity: 0.7455866083024681,
-      width: 1.603412060865523,
-    },
-    move: {
-      enable: true,
-      speed: 4,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "bounce",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 7776.548495197786,
-        rotateY: 1200,
-      },
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "repulse",
-      },
-      onclick: {
-        enable: true,
-        mode: "bubble",
-      },
-      resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 170.53621458328246,
-        size: 28.42270243054708,
-        duration: 0.6496617698410762,
-        opacity: 0.162415442460269,
-        /*speed: 3,*/
-      },
-      repulse: {
-        distance: 150,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
-      },
-    },
-  },
-  retina_detect: false,
-};
-
-const particleStyle = {
-  zIndex: 1,
-  position: "absolute",
-  top: 0,
-  bottom: 0,
-  left: 0,
-  margin: 0,
-  width: "100%",
-  height: "100%",
-};
 
 const Particle = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
@@ -141,14 +19,131 @@ const Particle = () => {
     },
     []
   );
-
   return (
     <Particles
       id="tsparticles"
-      options={particleOptions}
-      style={particleStyle}
       init={particlesInit}
       loaded={particlesLoaded}
+      style={{
+        zIndex: 1,
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        margin: 0,
+        width: "100%",
+        height: "100%",
+      }}
+      options={{
+        particles: {
+          number: {
+            value: 125,
+            density: {
+              enable: true,
+              value_area: 641.3648243462092,
+            },
+          },
+          color: {
+            value: "#17EFC8",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#ffffff",
+            },
+            polygon: {
+              nb_sides: 5,
+            },
+            image: {
+              src: "img/github.svg",
+              width: 100,
+              height: 100,
+            },
+          },
+          opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false,
+            },
+          },
+          size: {
+            value: 0,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 40,
+              size_min: 0.1,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#17EFC8",
+            opacity: 0.7455866083024681,
+            width: 1.603412060865523,
+          },
+          move: {
+            enable: true,
+            speed: 4,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "bounce",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 7776.548495197786,
+              rotateY: 1200,
+            },
+          },
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: {
+              enable: true,
+              mode: "repulse",
+            },
+            onclick: {
+              enable: true,
+              mode: "bubble",
+            },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 170.53621458328246,
+              size: 28.42270243054708,
+              duration: 0.6496617698410762,
+              opacity: 0.162415442460269,
+              /*speed: 3,*/
+            },
+            repulse: {
+              distance: 150,
+              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
+            },
+          },
+        },
+        retina_detect: false,
+      }}
     />
   );
 };
--- src/utility/Helpers.tsx
+++ src/utility/Helpers.tsx
@@ -16,7 +16,7 @@ export const shuffleArray = (array: any[]) => {
     randomIndex;
 
   // While there remain elements to shuffle.
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;

ace75f08e34a41ce524c1adcbd05a25315314557
Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>
2023-03-13 09:21:33 -0600

    fix events list
    Signed-off-by: Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>

--- src/components/EventsSection/EventInformation.ts
+++ src/components/EventsSection/EventInformation.ts
@@ -18,7 +18,7 @@ export const highlightedEvents: Event[] = [
   {
     title: "Final Showcase 2023",
     description:
-      "Passionate about tech or business strategy? Want to learn about current project at TechStart? Join us at Calgary Central Library for our Final Showcase where you will be able to network with over 20 industry experts and club members! ",
+      "Passionate about tech innovation or business strategy? Want to learn about current projects at Tech Start? Join us at our end-of-year Showcase where you will be able to network with over 20 industry experts and project teams! ",
     date: "Saturday, April 29, 2023",
     time: "12:00 - 3:30PM",
     image: FinalShowcase2023_img,
@@ -33,26 +33,6 @@ export const highlightedEvents: Event[] = [
     image: MicrosoftTechTalk_img,
     link: "https://youtu.be/a9MefhxE81w",
   },
-
-  {
-    title: "Microsoft Tech Talk",
-    description:
-      "We are very excited to be hosting our second tech talk: Meeting Microsoft!  Join us virtually Feb. 15th from 6:30 - 8:00 pm for an opportunity to hear from a Microsoft Software Developer, Software Engineering Manager, Product Manager, and Designer!",
-    date: "Wednesday, February 15, 2023",
-    time: "6:30 PM – 8:00 PM",
-    image: MicrosoftTechTalk_img,
-    link: "https://www.eventbrite.ca/e/meeting-microsoft-a-tech-talk-by-tech-start-tickets-536694346237",
-  },
-
-  {
-    title: "Blockchain Panel",
-    description:
-      "We hosted an enlightening panel discussion on the future of blockchain technology. Our esteemed panel of experts delved into the various applications and implications of this revolutionary technology. ",
-    date: "Friday, February 3, 2023",
-    time: "6:30 PM – 7:30 PM",
-    image: Blockchain_Panel_img,
-    link: "https://www.linkedin.com/feed/update/urn:li:activity:7027933893559271424",
-  },
   {
     title: "Interim Showcase 2023",
     description:
@@ -65,6 +45,15 @@ export const highlightedEvents: Event[] = [
 ];
 
 export const pastEvents: Event[] = [
+  {
+    title: "Blockchain Panel",
+    description:
+      "We hosted an enlightening panel discussion on the future of blockchain technology. Our esteemed panel of experts delved into the various applications and implications of this revolutionary technology. ",
+    date: "Friday, February 3, 2023",
+    time: "6:30 PM – 7:30 PM",
+    image: Blockchain_Panel_img,
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7027933893559271424",
+  },
   {
     title: "Arcurve PM Lunch and Learn",
     description:

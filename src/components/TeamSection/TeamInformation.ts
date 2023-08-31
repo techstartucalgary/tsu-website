d6498b47a143c9740a583b1bff601e9923a00a94
Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>
2023-08-30 19:13:56 -0600

    Updated team page
    Signed-off-by: Niyousha Raeesinejad <niyousha.raeesinejad@ucalgary.ca>

--- src/components/TeamSection/TeamInformation.ts
+++ src/components/TeamSection/TeamInformation.ts
@@ -6,8 +6,10 @@ import carol_jpg from "../../images/team/Carol_Wang.jpg";
 import rajpreet_jpg from "../../images/team/Rajpreet_Gill.jpg";
 import labib_jpg from "../../images/team/Labib_Ahmed.jpg";
 import nathan_jpg from "../../images/team/Nathan_Karbonik.jpg";
+import ana_jpg from "../../images/team/Ana_Laura.jpg";
 import estefy_jpeg from "../../images/team/Estefy_Caballero.jpeg";
 import enioluwafe_jpg from "../../images/team/Enioluwafe_Balogun.jpg";
+import maham_jpg from "../../images/team/Maham_Jamal.jpg";
 import maheen_jpg from "../../images/team/Maheen_Raza.jpg";
 import leo_jpg from "../../images/team/Leo_Wei.jpg";
 import rachel_jpg from "../../images/team/Rachel_Renegado.jpg";
@@ -15,6 +17,15 @@ import janita_jpg from "../../images/team/Janita_Mahum.jpg";
 import abod_jpg from "../../images/team/Abod_Abbas.jpg";
 import sajwal_jpg from "../../images/team/Sajwal Pageni.jpg";
 import ben_jpg from "../../images/team/Ben_Schmidt.jpg";
+import waika_jpg from "../../images/team/Wai_Ka_Wong.jpg";
+import sahiti_jpg from "../../images/team/Sahiti_Akella.jpg";
+import isaiah_jpg from "../../images/team/Isaiah_Asaolu.jpg";
+import aarsh_jpg from "../../images/team/Aarsh_Shah.jpg";
+import hilton_jpg from "../../images/team/Hilton_Luu.jpg";
+import wilbur_jpg from "../../images/team/Wilbur_Elbouni.jpg";
+import naman_jpg from "../../images/team/Naman_Bhoj.jpg";
+import lujaina_jpg from "../../images/team/Lujaina_ Eldelebshany.jpg";
+import hamza_jpg from "../../images/team/Hamza_Afzaal.jpg";
 import blank_jpg from "../../images/team/Blank.jpeg";
 
 export type TeamMember = {
@@ -55,46 +66,18 @@ export const executiveTeam: TeamMember[] = [
     linkedin: "https://www.linkedin.com/in/nathan-karbonik/",
   },
   {
-    id: 9,
-    name: "Leo Wei",
-    affiliation: "Co-VP Events",
-    image: leo_jpg,
-    linkedin: "https://www.linkedin.com/in/leo-wei/",
-  },
-  {
-    id: 10,
-    name: "Janita Mahum",
-    affiliation: "VP Design",
-    image: janita_jpg,
-    linkedin: "https://www.linkedin.com/in/janita-mahum-a5484a1b9/",
-  },
-  {
-    id: 13,
-    name: "Fedor Prokopchuk",
-    affiliation: "Business Strategy Advisor",
-    image: fedor_jpg,
-    linkedin: "https://www.linkedin.com/in/fedor-prokopchuk-707668a6/",
-  },
-  {
     id: 4,
+    name: "Ana Laura Espinosa Garza",
+    affiliation: "Jr. VP External",
+    image: ana_jpg,
+    linkedin: "https://www.linkedin.com/in/ana-laura-espinosa-garza-136a281b0/",
+  },
+  {
+    id: 5,
     name: "Carol Wang",
     affiliation: "VP Community",
     image: carol_jpg,
     linkedin: "https://www.linkedin.com/in/carol-wang3116/",
-  },
-  {
-    id: 5,
-    name: "Enioluwafe Balogun",
-    affiliation: "Co-VP Communications",
-    image: enioluwafe_jpg,
-    linkedin: "https://www.linkedin.com/in/enioluwafe-balogun/",
-  },
-  {
-    id: 11,
-    name: "Abod Abbas",
-    affiliation: "Senior Technical Advisor",
-    image: abod_jpg,
-    linkedin: "https://www.linkedin.com/in/abod-a-427274198/",
   },
   {
     id: 6,
@@ -103,16 +86,36 @@ export const executiveTeam: TeamMember[] = [
     image: estefy_jpeg,
     linkedin: "https://www.linkedin.com/in/estefy-caballero-864aab25a",
   },
-    {
-    id: 16,
-    name: "Ben Schmidt",
-    affiliation: "VP Development",
-    image: ben_jpg,
-    linkedin: "https://www.linkedin.com/in/benschmidt2343/",
-  },
-
   {
     id: 7,
+    name: "Enioluwafe Balogun",
+    affiliation: "Co-VP Communications",
+    image: enioluwafe_jpg,
+    linkedin: "https://www.linkedin.com/in/enioluwafe-balogun/",
+  },
+  {
+    id: 8,
+    name: "Maham Jamal",
+    affiliation: "Media Commissioner",
+    image: maham_jpg,
+    linkedin: "https://www.linkedin.com/in/maham-jamal-126a04258/",
+  },
+  {
+    id: 9,
+    name: "Leo Wei",
+    affiliation: "Co-VP Events",
+    image: leo_jpg,
+    linkedin: "https://www.linkedin.com/in/leo-wei/",
+  },
+  {
+    id: 10,
+    name: "Maheen Raza",
+    affiliation: "Co-VP Events",
+    image: maheen_jpg,
+    linkedin: "https://www.linkedin.com/in/maheen-raza-40b780229/",
+  },
+  {
+    id: 11,
     name: "Labib Afshar Ahmed",
     affiliation: "VP Finance",
     image: labib_jpg,
@@ -120,27 +123,71 @@ export const executiveTeam: TeamMember[] = [
   },
   {
     id: 12,
+    name: "Janita Mahum",
+    affiliation: "VP Design",
+    image: janita_jpg,
+    linkedin: "https://www.linkedin.com/in/janita-mahum-a5484a1b9/",
+  },
+  {
+    id: 13,
+    name: "Ben Schmidt",
+    affiliation: "VP Development",
+    image: ben_jpg,
+    linkedin: "https://www.linkedin.com/in/benschmidt2343/",
+  },
+  {
+    id: 14,
+    name: "Abod Abbas",
+    affiliation: "Senior Technical Advisor",
+    image: abod_jpg,
+    linkedin: "https://www.linkedin.com/in/abod-a-427274198/",
+  },
+  {
+    id: 15,
+    name: "Wai Ka Wong",
+    affiliation: "Technical Advisor",
+    image: waika_jpg,
+    linkedin: "https://www.linkedin.com/in/waika-wong/",
+  },
+
+  {
+    id: 16,
     name: "Sajwal Pageni",
     affiliation: "VP Strategy",
     image: sajwal_jpg,
     linkedin: "https://www.linkedin.com/in/sajwal/",
   },
   {
-    id: 8,
-    name: "Maheen Raza",
-    affiliation: "Co-VP Events",
-    image: maheen_jpg,
-    linkedin: "https://www.linkedin.com/in/maheen-raza-40b780229/",
+    id: 17,
+    name: "Fedor Prokopchuk",
+    affiliation: "Business Strategy Advisor",
+    image: fedor_jpg,
+    linkedin: "https://www.linkedin.com/in/fedor-prokopchuk-707668a6/",
   },
   {
-    id: 14,
+    id: 18,
+    name: "Sahiti Akella",
+    affiliation: "Web Developer",
+    image: sahiti_jpg,
+    linkedin: "https://www.linkedin.com/in/sahiti-akella/",
+  },
+  {
+    id: 19,
+    name: "Isaiah Asaolu",
+    affiliation: "Web Developer",
+    image: isaiah_jpg,
+    linkedin: "https://www.linkedin.com/in/isaiah-a-2001/",
+  },
+
+  {
+    id: 20,
     name: "Joel Happ",
     affiliation: "Founder & Chairman",
     image: joel_jpg,
     linkedin: "https://www.linkedin.com/in/joel-happ/",
   },
   {
-    id: 15,
+    id: 21,
     name: "Rajpreet Gill",
     affiliation: "Board Member",
     image: rajpreet_jpg,
@@ -151,9 +198,44 @@ export const executiveTeam: TeamMember[] = [
 export const projectManagers: TeamMember[] = [
   {
     id: 0,
-    name: "You?",
-    affiliation: "Apply now at techstartucalgary.com/apply!",
-    image: blank_jpg,
-    linkedin: " ",
+    name: "Aarsh Shah",
+    affiliation: "Project Manager",
+    image: aarsh_jpg,
+    linkedin: "https://www.linkedin.com/in/aarsh-shah-0a84161a9/",
+  },
+  {
+    id: 1,
+    name: "Hilton Luu",
+    affiliation: "Project Manager",
+    image: hilton_jpg,
+    linkedin: "https://www.linkedin.com/in/hilton-luu/",
+  },
+  {
+    id: 2,
+    name: "Lujaina Eldelebshany",
+    affiliation: "Project Manager",
+    image: lujaina_jpg,
+    linkedin: "https://www.linkedin.com/in/lujaina-eldelebshany-0029bb1b3/",
+  },
+  {
+    id: 3,
+    name: "Wilbur Elbouni",
+    affiliation: "Project Manager",
+    image: wilbur_jpg,
+    linkedin: "https://www.linkedin.com/in/wilbur-elbouni-3ba923213/",
+  },
+  {
+    id: 4,
+    name: "Naman Bhoj",
+    affiliation: "Project Manager",
+    image: naman_jpg,
+    linkedin: "https://www.linkedin.com/in/naman-b-3032a6154//",
+  },
+  {
+    id: 5,
+    name: "Hamza Afzaal",
+    affiliation: "Project Manager",
+    image: hamza_jpg,
+    linkedin: "https://www.linkedin.com/in/ammerhamza/",
   },
 ];

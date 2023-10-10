7ffe10f5e44430b707d7d2ca985463e2c9707d40
Abod Abbas <abdelrahman.abbas@ucalgary.ca>
2023-10-09 23:10:17 -0400

    adding guide markdown docs

--- src/components/DocsPageSections/GuidesSection.tsx
+++ src/components/DocsPageSections/GuidesSection.tsx
@@ -13,35 +13,35 @@ const GuidesSection = () => (
 
     <Guide
       guideName="Django Guide"
-      link="https://docs.google.com/document/d/1Emq59prVwyzpnOW8CkTfz9FEWE00AH0epGFHji2k_lA/edit?usp=sharing"
+      link="https://docs.techstartucalgary.com/guides/Django_Guide/index.html"
       image={imgDjango}
       description="Learn Django, a python-based web framework for building backends, APIs, multi-page applications, and more!"
     />
 
     <Guide
       guideName="Git Guide"
-      link="https://docs.google.com/document/d/19n16a4ksCs-MJmmru1TDtBWKjFOcfPF-bE8zuhR-RiQ/edit?usp=sharing"
+      link="https://docs.techstartucalgary.com/guides/Git_Guide/index.html"
       image={imgGit}
       description="Git is a fundamental tool for source control and collaborative development. This guide will help you learn and master git and GitHub workflows!"
-    />
+    />  
 
     <Guide
       guideName="Web Dev Guide"
-      link="https://docs.google.com/document/d/1K1prdp8otB_cowT4E39qM6SYlduTSG4g7abx3FtM3YA/edit?usp=sharing"
+      link="https://docs.techstartucalgary.com/guides/Web_Dev_Guide/index.html"
       image={imgWebDev}
       description="Learn the fundamentals of web development, with free resources and advice for mastering HTML, CSS, JS, and more!"
     />
 
     <Guide
       guideName="React Guide"
-      link="https://docs.google.com/document/d/1zR7VWVDxNo0xo5aTxR0EjeY1YgI1NIjT7wlC1GKE7bQ/edit"
+      link="https://docs.techstartucalgary.com/guides/React_Guide/index.html"
       image={imgReact}
       description="Learn the essentials of React, hooks, APIs, Redux, and React Native!"
     />
 
     <Guide
       guideName="API Guide"
-      link="https://docs.google.com/document/d/1ntKUGBy_6iPiZ1689uh4Q5C-crG5LhsXDKpY76WGwTE/edit"
+      link="https://docs.techstartucalgary.com/guides/GraphQL_API_Guide/index.html"
       image={imgApi}
       description="Learn about types of APIs, Rest APIs, Postman, and GraphQL!"
     />

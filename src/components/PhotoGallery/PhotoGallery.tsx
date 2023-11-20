3859552fb93c6ecc1278b96f538040955d039e22
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2023-11-20 08:34:01 -0700

    attempt to retrieve from google photos

--- src/components/PhotoGallery/PhotoGallery.tsx
+++ src/components/PhotoGallery/PhotoGallery.tsx
@@ -7,47 +7,50 @@ import useViewport from 'components/UseViewport';
 import { ImageListItem } from "@mui/material";
 import { GalleryImage, GalleryImages } from "./PhotoInformation";
 import { shuffleArray } from 'utility/Helpers';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const images = shuffleArray(GalleryImages);
 
 const PhotoGallery = () => {
-  /*
- const [images, setImages] = useState(GalleryImages);
 
- useEffect(() => {
-   // const clientID = '881622108956-r53m6cfoolr0c1kaidu4ib9eqkv3q3kk.apps.googleusercontent.com';
-   // const apiKey = 'AIzaSyDpXsNzgNPKqTsBi6c9-dLCWoc1J-Ps58U';
-   // const albumId = 'SkVei5N56poqTh8g8';
+  const [images, setImages] = useState(GalleryImages);
 
-   // fetchImagesFromAlbum(apiKey, albumId);
+  useEffect(() => {
+    // const clientID = '881622108956-r53m6cfoolr0c1kaidu4ib9eqkv3q3kk.apps.googleusercontent.com';
+    const apiKey = 'AIzaSyDpXsNzgNPKqTsBi6c9-dLCWoc1J-Ps58U';
+    const albumId = 'SkVei5N56poqTh8g8';
 
- }, []);
+    fetchImagesFromAlbum(apiKey, albumId);
 
- const fetchImagesFromAlbum = async (apiKey: string, albumId: string) => {
-   try {
-     const response = await axios.get(
-       `https://photoslibrary.googleapis.com/v1/mediaItems:search?albumId=${albumId}`,
-       {
-         headers: {
-           Authorization: `Bearer ${apiKey}`,
-         },
-       }
-     );
-     // eslint-disable-next-line
-     const images = response.data.mediaItems.map((item: any) => item.baseUrl);
-     setImages(images);
-   } catch (error) {
-     console.error('Error fetching images from Google Photos:', error);
-   }
- };
- */
+  }, []);
+
+  const fetchImagesFromAlbum = async (apiKey: string, albumId: string) => {
+    try {
+      const response = await axios.post(`https://photos.app.goo.gl/weUSkUpoxSRmALmo7`,
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+      console.log(response.data)
+      // eslint-disable-next-line
+      const images = response.data.mediaItems.map((item: any) => item.baseUrl);
+      setImages(images);
+    } catch (error) {
+      console.error('Error fetching images from Google Photos:', error);
+    }
+  };
+
 
   // set defaultView flag according to screen width cutoff value
   const { width } = useViewport(); // get screen width
   const cols = width > 600 ? 3 : 2;
 
   return (
-    <ImageList variant="woven" cols={cols} gap={10}>
+    <h1>Please work</h1>
+    /*<ImageList variant="woven" cols={cols} gap={10}>
       {images.map((image: GalleryImage) => (
         <ImageListItem key={image.id}>
           <img
@@ -58,7 +61,7 @@ const PhotoGallery = () => {
           />
         </ImageListItem>
       ))}
-    </ImageList>
+    </ImageList>*/
   );
 };
 

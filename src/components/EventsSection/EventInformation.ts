e8e5bc9cf941501e00382f2195c7d400a155628f
Sahiti Akella <63627104+Sahitiakella@users.noreply.github.com>
2024-01-17 15:01:36 -0700

    Added F23 events to homepage (#486)
    * Added F23 events
    
    Signed-off-by: Sahitiakella <sahiti.akella@gmail.com>
    
    * Updated IEEE event pic
    
    Signed-off-by: Sahitiakella <sahiti.akella@gmail.com>
    
    ---------
    
    Signed-off-by: Sahitiakella <sahiti.akella@gmail.com>

--- src/components/EventsSection/EventInformation.ts
+++ src/components/EventsSection/EventInformation.ts
@@ -1,6 +1,6 @@
-import MicrosoftTechTalk_img from "../../images/events/MicrosoftTechTalk.jpeg";
-import FinalShowcase2023_img from "../../images/events/final_showcase.jpg";
 import LaunchNight2023_img from "../../images/events/Launch_night_2023.jpeg";
+import PasonTechTalk_img from "../../images/events/PasonTechTalk.jpg";
+import IEEExTechStart_img from "../../images/events/IEEExTechStart.jpg";
 
 export interface Event {
   title: string;
@@ -13,30 +13,30 @@ export interface Event {
 
 export const highlightedEvents: Event[] = [
   {
+    title: "IEEE x TechStart",
+    description:
+      "We hosted our industry panel event in collaboration with IEEE on the topic of leveraging AI in product enhancement and user experiences! We covered real-world strategies and applications of AI in successful projects and had the opportunity to network with our industry guests from Galatea, GroupLabs, etc. ",
+    date: "Wednesday, November 29, 2023",
+    time: "5:00 - 7:00PM",
+    image: IEEExTechStart_img,
+    link: "https://www.instagram.com/p/C0SClrKPvWq/",
+  },
+  {
+    title: "Pason Tech Talk",
+    description:
+      "We hosted a Tech Talk featuring one of our sponsors, Pason! Our project managers and teams learned the key aspects of using infrastructure as code and possible implementations of this using terraform! Our project teams were given the opportunity to gain a practical understanding of IaC concepts and tools.",
+    date: "Thursday, November 9, 2023",
+    time: "5:30 - 7:00PM",
+    image: PasonTechTalk_img,
+    link: "https://www.linkedin.com/posts/tech-start-ucalgary_at-our-last-dev-night-we-hosted-a-tech-talk-activity-7130289774577254400-ESeU?utm_source=share&utm_medium=member_desktop",
+  },
+  {
     title: "Launch Night 2023",
     description:
-      "We kick off a thrilling new Tech Start Season! Our Project Managers pitched their projects and they're eager for you to be part of the action. Join us to learn more about what TechStart has in store for this year.",
+      "We kicked off a thrilling new Tech Start Season! Our Project Managers pitched their projects and they're eager for you to be part of the action. Join us to learn more about what TechStart has in store for this year.",
     date: "Friday, September 22, 2023",
     time: "5:30 - 7:00PM",
     image: LaunchNight2023_img,
     link: "https://www.instagram.com/p/CxjG39nSR0G/?hl=en",
-  },
-  {
-    title: "Final Showcase 2023",
-    description:
-      "We wrapped up Tech Start's 2022/2023 season with our final showcase event, where our project teams wowed our industry judges, sponsors, and special guests with their innovative solutions!",
-    date: "Saturday, April 29, 2023",
-    time: "12:00 - 3:30PM",
-    image: FinalShowcase2023_img,
-    link: "https://www.linkedin.com/posts/tech-start-ucalgary_softwareengineering-technology-programming-activity-7058565315751772161-hVgw?utm_source=share&utm_medium=member_desktop",
-  },
-  {
-    title: "Microsoft Tech Talk",
-    description:
-      "Our second tech talk featured Microsoft! We had the great opportunity to hear the stories of a Microsoft Software Developer, Software Engineering Manager, Product Manager, and Designer!",
-    date: "Wednesday, February 15, 2023",
-    time: "6:30 PM – 8:00 PM",
-    image: MicrosoftTechTalk_img,
-    link: "https://youtu.be/a9MefhxE81w",
   },
 ];

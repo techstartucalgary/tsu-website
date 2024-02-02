19bde01dcd8a81216113c33153e9a1c157e014d0
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-02-01 19:37:17 -0700

    added prettier

--- src/components/PhotoGallery/PhotoGallery.tsx
+++ src/components/PhotoGallery/PhotoGallery.tsx
@@ -1,43 +1,40 @@
-import axios from 'axios';
-import { useCallback, useEffect, useState } from 'react';
-import { PicturesContainer, Image } from './PhotoGallery.styles';
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { PicturesContainer, Image } from "./PhotoGallery.styles";
 
 const PhotoGallery = () => {
-  const [photosURL, setPhotosURL] = useState([]);// photos will be an array of objects
-
+  const [photosURL, setPhotosURL] = useState([]); // photos will be an array of objects
   //memoize a getAlbum to prevent unnecessary re-renders.
   const getAlbum = useCallback(async () => {
     const galleryPicsURL = process.env.REACT_APP_PIC_API_URL;
     try {
       if (!galleryPicsURL) {
-        throw new Error('URL is not defined');
+        throw new Error("URL is not defined");
       }
       const response = await axios.get(`${galleryPicsURL}/gallery`);
       const { data } = response;
       if (!data.length) {
-        throw new Error('No images found');
+        throw new Error("No images found");
       }
       setPhotosURL(data);
     } catch (error) {
-      console.error(`Error fetching images from the server on  ${galleryPicsURL}`, error);
+      console.error(
+        `Error fetching images from the server on  ${galleryPicsURL}`,
+        error
+      );
     }
   }, []);
 
   useEffect(() => {
     getAlbum();
-
   }, []);
   return (
     <PicturesContainer>
       {photosURL.map((photo, index) => (
-        <Image
-          src={photo}
-          key={index}
-        />
+        <Image src={photo} key={index} />
       ))}
     </PicturesContainer>
-  )
+  );
 };
 
 export default PhotoGallery;
-

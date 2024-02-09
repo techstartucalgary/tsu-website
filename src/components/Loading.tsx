c08e07a5bb8dc4163412c092a041d419e6945fe0
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-02-09 09:46:07 -0700

    Added a loading spinner
    Signed-off-by: IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,11 @@
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Loading = () =>{
 
     return(
-        <div style={{color:'white', textAlign:'center'}}>Loading...</div>
+        <div style={{color:'white', textAlign:'center'}}>
+            <CircularProgress size={70} color="success" />
+        </div>
     );
 };
 export default Loading;

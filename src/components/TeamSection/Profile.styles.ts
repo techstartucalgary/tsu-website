ffa334e72432d08820e8284b284ab532c31692b0
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-06-25 11:08:23 -0600

    Added object-fit: cover to profile images in TeamSection

--- src/components/TeamSection/Profile.styles.ts
+++ src/components/TeamSection/Profile.styles.ts
@@ -23,6 +23,7 @@ export const ProfileIconDiv = styled.div`
   & > img {
     width: 100%;
     height: 100%;
+    object-fit: cover;
     transition: 0.4s ease-in-out;
     background-size: cover;
     background-color: white;

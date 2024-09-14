91f6d1354a3173a8c0a182424dd83e5c741512b0
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-09-13 19:12:32 -0600

    Tests that had to find image elements were based on style-components which will regenerated each time. \n Hence, added data-testid to PhotoGallery.tsx, Profile.tsx, FounderSection.styles.ts to images could be uniquely identified based on an id.
    Signed-off-by: IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>

--- tests/navigationBar/NavBar.test.ts
+++ tests/navigationBar/NavBar.test.ts
@@ -5,7 +5,6 @@ import { test, expect } from '@playwright/test';
  */
 test('Test About button stays on the landing page', async ({ page }) => {
   await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
   await page.getByText('About').click();
   await expect(page).toHaveURL('https://techstartucalgary.com/');
   await page.close();
@@ -16,11 +15,11 @@ test('Test About button stays on the landing page', async ({ page }) => {
  * Test Team button navigates to the team page and shows Founder
  */
 test('Test Team button navigates to the team page and shows Founder', async ({ page }) => {
-  await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
+  // await page.goto('https://techstartucalgary.com/');
+  await page.goto('http://localhost:3000/')
   await page.getByText('Team', { exact: true }).click();
   await expect(page.getByRole('heading', { name: 'Our Team' })).toBeVisible();
-  await expect(page.locator('.FounderSectionstyles__FounderImg-sc-1pgvjm2-0')).toBeVisible();
+  await expect(page.getByTestId('founder-img')).toBeVisible();
   await expect(page.getByText('Joel Happ')).toBeVisible();
   await expect(page.getByText('Founder & Chairman')).toBeVisible();
   await expect(page.getByText('Joel HappFounder &')).toBeVisible();
@@ -33,13 +32,12 @@ test('Test Team button navigates to the team page and shows Founder', async ({ p
  */
 test('Test Team button navigates to the team page and shows the 3 previous presidents', async ({ page }) => {
   await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
   await page.getByText('Team', { exact: true }).click();
-  await expect(page.getByRole('img', { name: '/static/media/Niyousha_Raeesinejad.7d226e07871c60102716.jpg' })).toBeVisible();
+  await expect(page.getByTestId('profile-image-niyousha-raeesinejad')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Niyousha Raeesinejad' })).toBeVisible();
-  await expect(page.getByRole('img', { name: '/static/media/Tyler_Chan.' })).toBeVisible();
+  await expect(page.getByTestId('profile-image-tyler-chan')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Tyler Chan' })).toBeVisible();
-  await expect(page.getByRole('img', { name: '/static/media/Rajpreet_Gill.' })).toBeVisible();
+  await expect(page.getByTestId('profile-image-rajpreet-gill')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Rajpreet Gill' })).toBeVisible();
   await page.close();
 
@@ -49,13 +47,11 @@ test('Test Team button navigates to the team page and shows the 3 previous presi
  * Test Team button navigates to the team page and shows current presidents
  */
 test('Test Team button navigates to the team page and shows the 2 current presidents', async ({ page }) => {
-  await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
+ await page.goto('https://techstartucalgary.com/');
   await page.getByText('Team', { exact: true }).click();
-// await expect().toBeVisible();
-  await expect(page.getByRole('img', { name: '/static/media/Rachel_Renegado' })).toBeVisible();
+  await expect(page.getByTestId('profile-image-rachel-renegado')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Rachel Renegado' })).toBeVisible();
-  await expect(page.getByRole('img', { name: '/static/media/Aarsh_Shah.' })).toBeVisible();
+  await expect(page.getByTestId('profile-image-aarsh-shah')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Aarsh Shah' })).toBeVisible();
   await page.close();
 
@@ -66,7 +62,6 @@ test('Test Team button navigates to the team page and shows the 2 current presid
  */
 test('Test project button navigates to the project page', async ({ page }) => {
   await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
   await page.getByText('Projects', { exact: true }).click();
   await expect(page).toHaveURL('https://techstartucalgary.com/projects');
   await expect(page.getByText('Our', { exact: true })).toBeVisible();
@@ -81,7 +76,6 @@ test('Test project button navigates to the project page', async ({ page }) => {
  */
 test('Test apply button navigates to the apply page', async ({ page }) => {
   await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
   await page.getByText('Apply', { exact: true }).click();
   await expect(page).toHaveURL('https://techstartucalgary.com/apply');
   await expect(page.getByRole('heading', { name: 'APPLY' })).toBeVisible();
@@ -92,9 +86,8 @@ test('Test apply button navigates to the apply page', async ({ page }) => {
 /**
  * Test merch button navigates to the Merch page
  */
-test('Test merch button navigates to the Merch page and then find the 2 merch', async function({page}){
+test('Test merch button navigates to the Merch page and then find the 2 merch', async function ({ page }) {
   await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
   await page.getByText('Merch', { exact: true }).click();
   await expect(page).toHaveURL('https://techstartucalgary.com/merch');
   await expect(page.getByRole('heading', { name: 'Original Basic Crewneck' })).toBeVisible();
@@ -104,16 +97,15 @@ test('Test merch button navigates to the Merch page and then find the 2 merch', 
 });
 
 /**
- * Test gallery button navigates to the Gallery page
+ * Test that gallery button navigates to the Gallery page
  */
 test('Test gallery button navigates to the Merch page', async function ({ page }) {
   await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
   await page.getByText('Gallery', { exact: true }).click();
   await expect(page).toHaveURL('https://techstartucalgary.com/gallery');
   await page.getByRole('heading', { name: 'Gallery' }).click();
-  await expect(page.locator('.sc-beySbM > img').first()).toBeVisible();
-  await expect(page.locator('img:nth-child(15)')).toBeVisible();
+  await expect(page.getByTestId('photo-gallery-image-0')).toBeVisible();
+  await expect(page.getByTestId('photo-gallery-image-32')).toBeVisible();
   await page.close();
 });
 
@@ -122,7 +114,6 @@ test('Test gallery button navigates to the Merch page', async function ({ page }
  */
 test('Test resources button navigates to the Resources page', async function ({ page }) {
   await page.goto('https://techstartucalgary.com/');
-  await page.locator('label').click();
   await page.getByText('Resources', { exact: true }).click();
   await expect(page).toHaveURL('https://techstartucalgary.com/resources');
   await expect(page.getByText('Django Guide')).toBeVisible();

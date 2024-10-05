dacb14a6952ffd3c871229239ce8378d92b05121
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-10-05 17:08:17 -0600

    Refactor Playwright test workflow for Vercel deployment
    Signed-off-by: IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-reports', open: 'never' }],
+  ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */

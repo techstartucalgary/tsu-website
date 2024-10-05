4dfabd8a4d75889219496aa1597455392709886e
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-10-05 17:08:17 -0600

    Refactor Playwright test workflow for Vercel deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,26 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
-<<<<<<< HEAD
 import dotenv from 'dotenv';
 import path from 'path';
 
-=======
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
->>>>>>> b557bc4 (setup PlayWright tests)
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-<<<<<<< HEAD
   globalSetup: require.resolve('./global-setup'),
-=======
->>>>>>> b557bc4 (setup PlayWright tests)
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -31,22 +18,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-<<<<<<< HEAD
   reporter: [
     ['html', { outputFolder: 'playwright-reports', open: 'never' }],
     ['list']],
-=======
-  reporter: 'html',
->>>>>>> b557bc4 (setup PlayWright tests)
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-<<<<<<< HEAD
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
-=======
-
->>>>>>> b557bc4 (setup PlayWright tests)
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -55,38 +34,26 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-<<<<<<< HEAD
       use: {
         ...devices['Desktop Chrome'],
         viewport: { width: 1600, height: 900 }, // Simulating 90% zoom by using larger resolution
       },
-=======
-      use: { ...devices['Desktop Chrome'] },
->>>>>>> b557bc4 (setup PlayWright tests)
     },
 
     {
       name: 'firefox',
-<<<<<<< HEAD
       use: {
         ...devices['Desktop Firefox'],
         viewport: { width: 1600, height: 900 }, // Simulating 90% zoom by using larger resolution
       },
-=======
-      use: { ...devices['Desktop Firefox'] },
->>>>>>> b557bc4 (setup PlayWright tests)
     },
 
     {
       name: 'webkit',
-<<<<<<< HEAD
       use: {
         ...devices['Desktop Safari'],
         viewport: { width: 1600, height: 900 }, // Simulating 90% zoom by using larger resolution
       },
-=======
-      use: { ...devices['Desktop Safari'] },
->>>>>>> b557bc4 (setup PlayWright tests)
     },
 
     /* Test against mobile viewports. */

f036c74d669ce514405fb7ddc17abfd767be7f3d
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-10-05 14:43:52 -0600

    modify playwright.config.ts to  take the value passed from the GitHub Actions
    Signed-off-by: IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL: process.env.STAGING === '1' ? "http://localhost:3000" : "https://techstartucalgary.com",
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },

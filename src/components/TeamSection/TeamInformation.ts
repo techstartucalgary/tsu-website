644268b793a0e9c98c80262979e102a1e732ca1c
Sukritib13 <113867416+Sukritib13@users.noreply.github.com>
2024-10-06 17:57:26 -0600

    524 - initail changes, added Alumni tab, reusable components (#536)
    * 524 - initail changes, added Alumni tab, reusable components
    
    * 524-Adding back Slider to UI, addressing comments. Signed-off-by: Sukriti, Website Developer
    
    * Addressing comments - creating helper functions, fixing styles
    
    Signed-off-by: Sukritib13 <113867416+Sukritib13@users.noreply.github.com>
    
    * addressing comments - using var for bg color, using type for readability
    
    Signed-off-by: Sukritib13 <113867416+Sukritib13@users.noreply.github.com>
    
    * using types and correcting case
    
    Signed-off-by: Sukritib13 <113867416+Sukritib13@users.noreply.github.com>
    
    * using memoization
    
    Signed-off-by: Sukritib13 <113867416+Sukritib13@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sukritib13 <113867416+Sukritib13@users.noreply.github.com>
    Co-authored-by: brian nguyen <brian.nguy1en@gmail.com>
    Signed-off-by: IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>

--- src/components/TeamSection/TeamInformation.ts
+++ src/components/TeamSection/TeamInformation.ts
@@ -47,6 +47,12 @@ export const projectManagers: TeamMember[] = teamData.projectManagers?.length ?
     linkedin: ""
   }];
 
+export const alumniTeam: TeamMember[] = teamData.alumniTeam.map((alum, index) => ({
+  id: index,
+  ...alum,
+  image: importImage(alum.imagePath)
+}));
+
 export const boardMembers: TeamMember[] = teamData.boardMembers.map((bm, index) => ({
   id: index,
   ...bm,
--- src/components/TeamSection/TeamSection.styles.ts
+++ src/components/TeamSection/TeamSection.styles.ts
@@ -1,4 +1,19 @@
 import styled from "styled-components/macro";
+import { TeamCategory } from "./Team";
+
+// helper function 
+const getStylesForCategory = (category: TeamCategory) => {
+  switch (category) {
+    case "executives":
+      return { backgroundColor: "var(--primary-green)", left: "5px" };
+    case "projectManagers":
+      return { backgroundColor: "var(--lightwash-green)", left: "calc(33.3% + 5px)" };
+    case "alumni":
+      return { backgroundColor: "var(--secondary-lime)", left: "calc(66.6% + 5px)" };
+    default:
+      return { backgroundColor: "var(--primary-green)", left: "5px" }; // Default to executives
+  }
+};
 
 export const TeamSection = styled.div`
   padding: 5vw;
@@ -19,102 +34,56 @@ export const TeamSection = styled.div`
 `;
 
 export const ToggleButtonWrapper = styled.div`
+  display: flex;
   position: relative;
   margin-top: 2%;
   margin-left: -5%;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const ToggleButtonLabel = styled.label`
-  overflow-y: hidden;
-  display: inline-block;
+export const SliderWrapper = styled.div<{ selectedCategory: TeamCategory}>`
   position: relative;
-  @media (min-width: 351px) and (max-width: 475px) {
-    width: 280px;
-    height: 40px;
-    margin-left: 0%;
-    margin-top: 5%;
-  }
-  @media (min-width: 320px) and (max-width: 350px) {
-    width: 280px;
-    height: 40px;
-    margin-left: 5%;
-    margin-top: -5%;
-  }
-  margin-left: 0%;
-  width: 425px;
-  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  width: 700px;
+  height: 60px;
+  background-color: ${({ selectedCategory }) =>
+  selectedCategory === "executives" ? "var(--primary-green)" :
+    selectedCategory === "projectManagers" ? "var(--lightwash-green)" :
+        "var(--secondary-lime)"};
   border-radius: 50px;
-  background: #4dd6a8;
+  padding: 5px;
+`;
+
+export const SliderOption = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
   cursor: pointer;
-  &::after {
-    transform: translate(200px, -35px);
-    content: "";
-    display: block;
-    border-radius: 50px;
-    background-color: #49b893;
-    @media (min-width: 320px) and (max-width: 475px) {
-      width: 140px;
-      height: 30px;
-      margin-left: -65px;
-      margin-top: 23px;
-    }
-    width: 210px;
-    height: 40px;
-    margin-left: 10px;
-    margin-top: -57px;
-    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
-    transition: 0.8s;
-  }
-`;
-
-export const ToggleButton = styled.input`
-  opacity: 0;
-  z-index: 1;
-  border-radius: 15px;
-  width: 42px;
-  height: 26px;
-  &:checked + ${ToggleButtonLabel} {
-    background: #61c3d4;
-    &::after {
-      content: "";
-      border-radius: 50px;
-      background-color: #59acba;
-      width: 200px;
-      height: 40px;
-      @media (min-width: 320px) and (max-width: 475px) {
-        width: 130px;
-        height: 30px;
-        margin-left: 5px;
-        margin-top: 23px;
-      }
-      margin-left: 5px;
-      margin-top: -57px;
-      transition: 0.8s;
-      transform: translateY(-35px);
-    }
-  }
-`;
-
-export const TeamCategoryText = styled.p`
-  font-size: 1em;
-  margin-left: -20px;
-  font-family: "Inter", Tahoma, sans-serif;
-  color: white;
+  padding: 10px 5px;
   font-weight: 500;
-  
-  @media (min-width: 320px) and (max-width: 475px) {
-    font-size: 0.65em;
-    color: white;
+  font-size: 1.5em;
+  color: #fff;
+  z-index: 2;
+
+  @media (max-width: 700px) {
+    font-size: 1.2em;
   }
 `;
 
-export const Slider = styled.p`
-  font-size: 1em;
-  margin-left: 200px;
-  height: 24px;
-  @media (max-width: 475px) {
-    height: 0;
-    margin-top: -10px;
-  }
-  font-weight: 500;
+export const SliderPosition = styled.div<{ selectedCategory: TeamCategory }>`
+  position: absolute;
+  top: 5px;
+  left: ${({ selectedCategory }) =>
+    getStylesForCategory(selectedCategory).left};
+  width: calc(33.3% - 10px);
+  height: calc(100% - 10px);
+  background-color: ${({ selectedCategory }) =>
+    getStylesForCategory(selectedCategory).backgroundColor};
+  border-radius: 50px;
+  box-shadow: rgba(0, 0, 0, 0.2) 3px 3px 3px 3px;
+  transition: left 0.8s ease, background-color 0.2s ease-in-out;
 `;

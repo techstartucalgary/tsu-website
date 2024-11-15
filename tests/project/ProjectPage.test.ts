3c91ab2be9fff90247078898d2dd74de26e21d04
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-11-14 17:44:00 -0700

    Everything working as expected

--- tests/project/ProjectPage.test.ts
+++ tests/project/ProjectPage.test.ts
@@ -3,20 +3,11 @@ import { test, expect } from '@playwright/test';
 /**
  * Test 'Apply Now' nagivates to the apply now page
  */
-<<<<<<< HEAD
 test('Apply Now btn nagivates to the apply now page', async ({ page, baseURL }) => {
   await page.goto(`${baseURL}/projects`);
   const applyButton = page.getByTestId('apply_now_button');
-  applyButton.click();
+  await applyButton.click();
   await expect(page).toHaveURL(`${baseURL}/apply`);
   // await expect(page.getByRole('heading', { name: 'APPLY' })).toBeVisible();
-=======
-test('Apply Now btn nagivates to the apply now page', async ({ page }) => {
-  await page.goto('https://techstartucalgary.com/projects');
-  await page.getByText('Apply Now').click();
-  await expect(page).toHaveURL(/.*apply/);
-  await expect(page.getByRole('heading', { name: 'APPLY' })).toBeVisible();
->>>>>>> e548a21 (Added test coverage for navigation bar and project page)
   await page.close();
-
 });

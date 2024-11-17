e8c74e67966e31cc554fa551e081eaaf202bbee2
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-11-16 18:01:21 -0700

    Ran prettier

--- src/components/PreviousExecTeam/PreviousExecTeam.tsx
+++ src/components/PreviousExecTeam/PreviousExecTeam.tsx
@@ -3,8 +3,11 @@ import * as S from "./PreviousExecTeam.styles";
 import prevTeamsData from "./PreviousExecTeamsInfo.json";
 // https://react-slick.neostack.com/docs/example/
 import Divider from "components/Divider";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface yearData {
   year: string;
@@ -24,34 +27,43 @@ interface PreviousExecTeamProps {
   desktopView: boolean;
 }
 const PreviousExecTeam = (props: PreviousExecTeamProps) => {
-  const [selectedYear, setSelectedYear] = useState(prevTeamsData[prevTeamsData.length - 1].year);
+  const [selectedYear, setSelectedYear] = useState(
+    prevTeamsData[prevTeamsData.length - 1].year
+  );
   const [visibleYears, setVisibleYears] = useState<yearData[]>([]);
 
   useEffect(() => {
     updateVisibleYears();
-  }, [props.desktopView]);// only run on inital render or when view changes
+  }, [props.desktopView]); // only run on inital render or when view changes
 
   // Function to set the visible years
   function updateVisibleYears() {
     const selectedYearIndex = getSelectedYearIndex();
-    const numOfVisibleYears = props.desktopView ? VISIBLE_YEARS_DESKTOP : VISIBLE_YEARS_MOBILE;
+    const numOfVisibleYears = props.desktopView
+      ? VISIBLE_YEARS_DESKTOP
+      : VISIBLE_YEARS_MOBILE;
 
     // Calculate startIndex based on the selectedYearIndex
     let startIndex = Math.max(0, prevTeamsData.length - numOfVisibleYears);
     // If the selected year is near the beginning, adjust startIndex to show available years
     if (selectedYearIndex < startIndex) {
-      startIndex = Math.max(0, selectedYearIndex - Math.floor(numOfVisibleYears / 2));
+      startIndex = Math.max(
+        0,
+        selectedYearIndex - Math.floor(numOfVisibleYears / 2)
+      );
     }
 
     // Calculate endIndex based on startIndex
-    const endIndex = Math.min(prevTeamsData.length, startIndex + numOfVisibleYears);
+    const endIndex = Math.min(
+      prevTeamsData.length,
+      startIndex + numOfVisibleYears
+    );
 
     setVisibleYears(prevTeamsData.slice(startIndex, endIndex));
   }
 
-  const getSelectedYearIndex = (): number => 
+  const getSelectedYearIndex = (): number =>
     prevTeamsData.findIndex((data) => data.year === selectedYear);
-  
 
   const handleLeftArrow = () => {
     const selectedYearIndex = getSelectedYearIndex();
@@ -59,13 +71,19 @@ const PreviousExecTeam = (props: PreviousExecTeamProps) => {
       setSelectedYear(prevTeamsData[selectedYearIndex - 1].year);
 
       // Recalculate visible years only if necessary
-      const currentStartIndex = prevTeamsData.findIndex((data) => data.year === visibleYears[0].year);
+      const currentStartIndex = prevTeamsData.findIndex(
+        (data) => data.year === visibleYears[0].year
+      );
 
       // if the selectedyear index less that the start of the visible year array
       if (selectedYearIndex - 1 < currentStartIndex) {
         // Only shift the visible years by 1 year to the left so we don't introduce any unnecssary years
         const newStartIndex = Math.max(0, currentStartIndex - 1);
-        const newEndIndex = Math.min(prevTeamsData.length, newStartIndex + (props.desktopView ? VISIBLE_YEARS_DESKTOP : VISIBLE_YEARS_MOBILE));
+        const newEndIndex = Math.min(
+          prevTeamsData.length,
+          newStartIndex +
+            (props.desktopView ? VISIBLE_YEARS_DESKTOP : VISIBLE_YEARS_MOBILE)
+        );
         setVisibleYears(prevTeamsData.slice(newStartIndex, newEndIndex));
       }
     }
@@ -76,19 +94,28 @@ const PreviousExecTeam = (props: PreviousExecTeamProps) => {
       setSelectedYear(prevTeamsData[selectedYearIndex + 1].year);
 
       // Recalculate visible years only if necessary
-      const currentStartIndex = prevTeamsData.findIndex((data) => data.year === visibleYears[0].year);
-      const numOfVisibleYears = props.desktopView ? VISIBLE_YEARS_DESKTOP : VISIBLE_YEARS_MOBILE;
+      const currentStartIndex = prevTeamsData.findIndex(
+        (data) => data.year === visibleYears[0].year
+      );
+      const numOfVisibleYears = props.desktopView
+        ? VISIBLE_YEARS_DESKTOP
+        : VISIBLE_YEARS_MOBILE;
 
       // If the selected year moves beyond the currently visible years
       if (selectedYearIndex + 1 >= currentStartIndex + numOfVisibleYears) {
         // Shift the visible years by one to the right
-        const newStartIndex = Math.min(prevTeamsData.length - numOfVisibleYears, currentStartIndex + 1);
-        const newEndIndex = Math.min(prevTeamsData.length, newStartIndex + numOfVisibleYears);
+        const newStartIndex = Math.min(
+          prevTeamsData.length - numOfVisibleYears,
+          currentStartIndex + 1
+        );
+        const newEndIndex = Math.min(
+          prevTeamsData.length,
+          newStartIndex + numOfVisibleYears
+        );
         setVisibleYears(prevTeamsData.slice(newStartIndex, newEndIndex));
       }
     }
   };
-
 
   return (
     <S.PrevTeamSection>
@@ -100,13 +127,17 @@ const PreviousExecTeam = (props: PreviousExecTeamProps) => {
           onClick={handleLeftArrow}
           disabled={getSelectedYearIndex() === 0}
         >
-        <FontAwesomeIcon icon={faChevronLeft} size="sm" style={{color:"black"}} />
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            size="sm"
+            style={{ color: "black" }}
+          />
         </S.ArrowButton>
         <S.YearPagination>
           {visibleYears.map((data) => (
             <S.YearButton
               key={data.year}
-              className={selectedYear === data.year ? 'selected' : ''}
+              className={selectedYear === data.year ? "selected" : ""}
               onClick={() => setSelectedYear(data.year)}
             >
               {data.year}
@@ -118,46 +149,49 @@ const PreviousExecTeam = (props: PreviousExecTeamProps) => {
           onClick={handleRightArrow}
           disabled={getSelectedYearIndex() === prevTeamsData.length - 1}
         >
-        <FontAwesomeIcon icon={faChevronRight} size="sm" style={{color:"black"}} />
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            size="sm"
+            style={{ color: "black" }}
+          />
         </S.ArrowButton>
       </S.PaginationControl>
-      
+
       <S.Divider />
-      
+
       <S.TeamList>
         {prevTeamsData
           .filter((team) => team.year === selectedYear)
           .map((team) => {
             const totalMember = team.members.length;
             const membersPerRow = props.desktopView ? 2 : 1; // based on the view, how many columns
-            const lastRowStartIndex = Math.floor((totalMember-1)/membersPerRow)*membersPerRow; // index of element that will be on the last row
-            return(
-            <>
-              {team.members.length > 0 && (
-                <>
-                  {team.members.map((member, index) => (
-                    <S.TeamMember 
-                      key={index}
-                      lastRow={index>= lastRowStartIndex ? true : false} // if index is greater or equal, than we are on last row
-                    >
-                      <S.TeamHeader
-                      href={member.linkedin_url}
-                      target="_blank" 
-                      rel="noopener noreferrer"
+            const lastRowStartIndex =
+              Math.floor((totalMember - 1) / membersPerRow) * membersPerRow; // index of element that will be on the last row
+            return (
+              <>
+                {team.members.length > 0 && (
+                  <>
+                    {team.members.map((member, index) => (
+                      <S.TeamMember
+                        key={index}
+                        lastRow={index >= lastRowStartIndex ? true : false} // if index is greater or equal, than we are on last row
                       >
-                        {member.name}
-                      </S.TeamHeader>
-                      <S.TeamRole>{member.role}</S.TeamRole>
-                    </S.TeamMember>
-                  ))}
-                </>
-              )}
-            </>
-          )
-}
-)}
+                        <S.TeamHeader
+                          href={member.linkedin_url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {member.name}
+                        </S.TeamHeader>
+                        <S.TeamRole>{member.role}</S.TeamRole>
+                      </S.TeamMember>
+                    ))}
+                  </>
+                )}
+              </>
+            );
+          })}
       </S.TeamList>
-
     </S.PrevTeamSection>
   );
 };
